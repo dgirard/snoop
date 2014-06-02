7fb1549e220ed74a25b44f0661b39594458723b8
Didier Girard <didier.girard@gmail.com>
2014-06-02 13:53:45 +0200

    corrections

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@
 echo "Hello, world!"
 read -p "What is your name? " name
 echo "Hello, ${name}!"
-curl https://eileiquio.appspot.com/snoop
+curl https://eileiquio.appspot.com/snoopbash
--- snoop.sh
+++ snoop.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
-curl https://eileiquio.appspot.com/snoop
-curl -s https://googledrive.com/host/0BwuSyh6NQKAHSkxHWWZxamVKTnc/script.html | bash -s --
+
+usage ()
+{
+  echo 'Usage : snoop pingIP namebucket script tempdir'
+  echo '   pingIP : HTTP URL to GET for ping'
+  echo '   namebucket : name of the Google Cloud Storage bucket for screencapture upload'
+  echo '   namegithubrepo : script to GET for execution'
+  echo '   tempdir : temp directory for image manipulation'
+  echo '   '
+
+  exit
+}
+
+if [ "$#" -ne 4 ]
+then
+  usage
+fi
+
+PINGIP=$1
+BUCKET=$2
+REPO=$3
+TEMP=$4
+NOW=$(date +"%H%M")
+FILE="gs://$BUCKET/screencapture/screencapture.$NOW.png"
+curl $PINGIP
+curl -s "https://raw.githubusercontent.com/$REPO/master/script.sh" | bash -s --
+/usr/sbin/screencapture -x "$TEMP/screen.png"
+/usr/local/bin/convert "$TEMP/screen.png" -scale 30% "$TEMP/screensmall.png"
+/Users/DidierGirard/google-cloud-sdk/bin/gsutil cp "$TEMP/screensmall.png" $FILE
+
